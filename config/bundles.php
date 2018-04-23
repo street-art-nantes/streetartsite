4b0b17a83e63e4f1a99f29e40d48c655fe5e9ce3
Julien PETIT <contact@julienpetit.fr>
2018-04-23 21:28:53 +0200

    Install following bundles : * ApiPlatform * Logger * Console * VichUploader * Stof/doctrine-extensions * php/translations

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,9 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Translation\Bundle\TranslationBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
