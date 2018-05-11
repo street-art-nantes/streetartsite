44cf06436b7979d22a150ad5eecb1a05bba7994c
Julien PETIT <contact@julienpetit.fr>
2018-05-11 16:59:04 +0200

    install fos user bundle

---
+++ src/Migrations/Version20180511145300.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180511145300 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER SEQUENCE document_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE poi_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE artwork_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE author_id_seq INCREMENT BY 1');
+        $this->addSql('CREATE SEQUENCE fos_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE fos_user (id INT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled BOOLEAN NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, roles TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A647992FC23A8 ON fos_user (username_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479A0D96FBF ON fos_user (email_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A6479C05FB297 ON fos_user (confirmation_token)');
+        $this->addSql('COMMENT ON COLUMN fos_user.roles IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SCHEMA topology');
+        $this->addSql('CREATE SCHEMA tiger');
+        $this->addSql('CREATE SCHEMA tiger_data');
+        $this->addSql('ALTER SEQUENCE document_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE poi_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE author_id_seq INCREMENT BY 1');
+        $this->addSql('ALTER SEQUENCE artwork_id_seq INCREMENT BY 1');
+        $this->addSql('DROP SEQUENCE fos_user_id_seq CASCADE');
+        $this->addSql('DROP TABLE fos_user');
+    }
+}
