7f26443d31daaeb3186118b1c61d3ef77040bac8
Julien PETIT <contact@julienpetit.fr>
2018-05-12 11:59:45 +0200

    update config

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=610eef16da5a91eba80cc237c8872b6564dae5fa\
+    --form revision=d40c1b7da8ef86e4a7befd0b95df4d39b85cb7a6\
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/street-art-nantes/streetartsite/tree/master
+        https://circleci.com/api/v1.1/project/github/street-art-nantes/streetartsite/tree/feature/ci
