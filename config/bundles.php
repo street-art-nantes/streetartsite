9973f125679228c92ed28845173a8420a47cd695
Julien PETIT <contact@julienpetit.fr>
2018-05-12 15:38:36 +0200

    install easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
