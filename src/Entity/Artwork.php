e25951338c968f460ec98f9a1d9d372cf3a45631
Julien PETIT <contact@julienpetit.fr>
2018-06-28 12:39:35 +0200

    Set artwork title nullable

--- src/Entity/Artwork.php
+++ src/Entity/Artwork.php
@@ -22,7 +22,7 @@ class Artwork
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $title;
 
