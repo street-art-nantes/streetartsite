b619aac7db75d2b00d6817d1cecbeff62d71b807
Rabot <arabottin@gmail.com>
2018-09-10 22:01:43 +0200

    change map zoom

--- assets/js/map.js
+++ assets/js/map.js
@@ -16,7 +16,7 @@ const resizeContentMapHeight = () => {
 }
 
 const initMap = () => {
-  var map = L.map('map', { scrollWheelZoom:false }).setView([46.506755997519654, 7.699578983615311], 5);
+  var map = L.map('map', { scrollWheelZoom:false }).setView([30, 8], 2);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
