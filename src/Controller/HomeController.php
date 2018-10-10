5a5b759f512be013a8781696737cc7ff5c50d44e
Julien PETIT <contact@julienpetit.fr>
2018-10-10 22:10:38 +0200

    Fix php cs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -38,12 +38,12 @@ class HomeController extends Controller
         $countriesFromPoi = $poiRepository->getAllCountries();
 
         $columnCount = 3;
-        $colPois = array_chunk($pois, ceil(count($pois) / $columnCount));
+        $colPois = array_chunk($pois, ceil(\count($pois) / $columnCount));
 
         return $this->render('pages/home.html.twig', [
             'colPois' => $colPois,
             'totalPois' => $totalPois,
-            'totalCountry' => count($countriesFromPoi),
+            'totalCountry' => \count($countriesFromPoi),
             'topContributor' => $topContributor,
         ]);
     }
