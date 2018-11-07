38e7e8c2e3eeba05252ce595da423bc974a32982
Rabot <arabottin@gmail.com>
2018-11-07 23:06:43 +0100

    artist list and details

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,13 +2,22 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AuthorRepository")
+ * @Vich\Uploadable
  */
 class Author
 {
+
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue(strategy="IDENTITY")
@@ -27,9 +36,48 @@ class Author
     private $biography;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $websiteLink;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $instagramLink;
+
+    /**
+     * @Vich\UploadableField(mapping="user_avatar", fileNameProperty="avatarName")
+     * @Assert\File(
+     *     maxSize = "2M",
+     *     mimeTypes = {"image/jpeg"},
+     *     mimeTypesMessage = "Please upload a valid JPG"
+     * )
+     *
+     * @var File
+     */
+    private $avatarFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $avatarName;
+
+    /**
      * @ORM\OneToMany(targetEntity="Artwork", mappedBy="author")
      */
-    private $artwork;
+    private $artworks;
+
+    /**
+     * Author constructor.
+     */
+    public function __construct()
+    {
+        $this->artworks = new ArrayCollection();
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
 
     public function getId()
     {
@@ -61,22 +109,86 @@ class Author
     }
 
     /**
-     * @return mixed
+     * @return File
      */
-    public function getArtwork()
+    public function getAvatarFile(): ?File
     {
-        return $this->artwork;
+        return $this->avatarFile;
     }
 
     /**
-     * @param mixed $artwork
+     * @param File $avatarFile
      *
      * @return Author
      */
-    public function setArtwork($artwork)
+    public function setAvatarFile(File $avatarFile): self
     {
-        $this->artwork = $artwork;
+        $this->avatarFile = $avatarFile;
+
+        if (null !== $avatarFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime();
+        }
 
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function getAvatarName(): ?string
+    {
+        return $this->avatarName;
+    }
+
+    /**
+     * @param string $avatarName
+     *
+     * @return Author
+     */
+    public function setAvatarName(string $avatarName): self
+    {
+        $this->avatarName = $avatarName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebsiteLink()
+    {
+        return $this->websiteLink;
+    }
+
+    /**
+     * @param mixed $websiteLink
+     * @return Author
+     */
+    public function setWebsiteLink($websiteLink)
+    {
+        $this->websiteLink = $websiteLink;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInstagramLink()
+    {
+        return $this->instagramLink;
+    }
+
+    /**
+     * @param mixed $instagramLink
+     * @return Author
+     */
+    public function setInstagramLink($instagramLink)
+    {
+        $this->instagramLink = $instagramLink;
+        return $this;
+    }
+
+
 }
