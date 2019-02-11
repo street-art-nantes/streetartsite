cd650a62b125acaacadbefc53b1870912370c57a
Rabot <arabottin@gmail.com>
2019-02-11 17:19:51 +0100

    blog display

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -74,7 +74,8 @@ class BlogController extends Controller
         }
 
         $query = new Query();
-        $query->setLocale($localeArray[$request->getLocale()])->setContentType('blogPost');
+        $query->setLocale($localeArray[$request->getLocale()])->setContentType('blogPost')
+        ->orderBy('sys.updatedAt', true);
 
         try {
             $entries = $client->getEntries($query);
