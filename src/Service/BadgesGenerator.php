c5d464d8bbd1ed90335d723f0370c5d542a845ee
Rabot <arabottin@gmail.com>
2019-03-14 12:27:45 +0100

    create mail

--- src/Service/BadgesGenerator.php
+++ src/Service/BadgesGenerator.php
@@ -57,16 +57,22 @@ class BadgesGenerator
     protected $user;
 
     /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    /**
      * BadgesGenerator constructor.
      * @param UserRepository $userRepository
      * @param ArtworkRepository $artworkRepository
      * @param AuthorRepository $authorRepository
      * @param EntityManagerInterface $manager
      * @param LoggerInterface $logger
+     * @param Mailer $mailer
      * @param array $parameters
      */
     public function __construct(UserRepository $userRepository, ArtworkRepository $artworkRepository, AuthorRepository $authorRepository,
-                                EntityManagerInterface $manager, LoggerInterface $logger, array $parameters)
+                                EntityManagerInterface $manager, LoggerInterface $logger, Mailer $mailer, array $parameters)
     {
         $this->userRepository = $userRepository;
         $this->artworkRepository = $artworkRepository;
@@ -74,6 +80,7 @@ class BadgesGenerator
         $this->manager = $manager;
         $this->parameters = $parameters;
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
 
     /**
@@ -121,14 +128,22 @@ class BadgesGenerator
     {
         $encoders = [new XmlEncoder(), new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
-
         $serializer = new Serializer($normalizers, $encoders);
+        $newBadges = [];
+
         /**
          * @var BadgesUser $userBadges
          */
         $userBadges = $serializer->deserialize($this->user->getBadges(), BadgesUser::class, 'json');
         echo 'level depuis la bdd : '.$userBadges->getArtworkLevel();
         echo 'level depuis le calcul : '.$this->badgeUser->getArtworkLevel();
+
+        if ($userBadges->getArtworkLevel() != $this->badgeUser->getArtworkLevel()) $newBadges['artwork'] = $this->badgeUser->getArtworkLevel();
+        if ($userBadges->getArtistLevel() != $this->badgeUser->getArtistLevel()) $newBadges['artist'] = $this->badgeUser->getArtistLevel();
+        if ($userBadges->getCityLevel() != $this->badgeUser->getCityLevel()) $newBadges['city'] = $this->badgeUser->getCityLevel();
+        if ($userBadges->getCountryLevel() != $this->badgeUser->getCountryLevel()) $newBadges['country'] = $this->badgeUser->getCountryLevel();
+
+        if ($newBadges) $this->mailer->sendNewBadgesEmailMessage($this->user, $newBadges);
     }
 
     private function generateArtworkBadge()
