5e843638ff50f23045a19da131fa26a5f4c6bce2
rabot18 <g7xvuftq>
2019-03-19 17:19:35 +0100

    add artwork type in submit form

--- src/Form/Type/ArtworkType.php
+++ src/Form/Type/ArtworkType.php
@@ -7,6 +7,7 @@ use App\Entity\Author;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,6 +27,15 @@ class ArtworkType extends AbstractType
         $builder->add('title', TextType::class, [
             'label' => 'artwork.label.title',
             'required' => false,
+        ]);
+        $builder->add('type', ChoiceType::class, [
+            'label' => 'artwork.label.type',
+            'required' => true,
+            'choices'  => [
+                'artwork.label.graffiti' => Artwork::TYPE_GRAFFITI,
+                'artwork.label.sticking' => Artwork::TYPE_STICKING,
+                'artwork.label.mosaic' => Artwork::TYPE_MOSAIC,
+            ],
         ]);
         $builder->add('poi', PoiType::class, [
             'label' => false,
