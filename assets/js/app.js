d4dc9b7639a5e5fe6e76a9fc1f0e5b80f93bfd69
rabot18 <arabottin@gmail.com>
2019-06-07 15:25:49 +0200

    add exit popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,22 @@ import "./components/field-image"
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+// exit popup
+function addEvent(obj, evt, fn) {
+  if (obj.addEventListener) {
+    obj.addEventListener(evt, fn, false);
+  }
+  else if (obj.attachEvent) {
+    obj.attachEvent("on" + evt, fn);
+  }
+}
+
+addEvent(document, 'mouseout', function(evt) {
+  if (!evt.toElement && !evt.relatedTarget /*&& !localStorage.getItem('exitintent_show')*/) {
+    console.log("left window");
+    $('#follow-us').modal()
+    //localStorage.setItem('exitintent_show', 'true');
+  }
+});
