f9a4556805623113d2fbcfcfe47df217d6fe7e9a
Julien PETIT <contact@julienpetit.fr>
2019-09-26 22:46:13 +0200

    Slack notifications

--- src/Listener/RegistrationSuccessListener.php
+++ src/Listener/RegistrationSuccessListener.php
@@ -2,20 +2,40 @@
 
 namespace App\Listener;
 
+use App\Entity\User;
 use App\Service\Mailer;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use FOS\UserBundle\FOSUserEvents;
+use Nexy\Slack\Client;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class RegistrationSuccessListener implements EventSubscriberInterface
 {
+    /**
+     * @var Mailer
+     */
     private $mailer;
 
-    public function __construct(Mailer $mailer)
+    /**
+     * @var Client
+     */
+    private $slackClient;
+
+    /**
+     * RegistrationSuccessListener constructor.
+     *
+     * @param Mailer $mailer
+     * @param Client $slackClient
+     */
+    public function __construct(Mailer $mailer, Client $slackClient)
     {
         $this->mailer = $mailer;
+        $this->slackClient = $slackClient;
     }
 
+    /**
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return [
@@ -30,8 +50,27 @@ class RegistrationSuccessListener implements EventSubscriberInterface
      */
     public function onRegistrationSuccess(FilterUserResponseEvent $event)
     {
+        /** @var User $user */
         $user = $event->getUser();
 
-        $this->mailer->sendWelcomeEmailMessage($user);
+        try {
+            $message = $this->slackClient->createMessage();
+
+            $message
+                ->to('#new-users')
+                ->from('Street Artwork bot')
+                ->withIcon(':genie:')
+                ->setText(sprintf('%s rejoint Street Artwork! Ville: %s Pays:%s', $user->getUsername(), $user->getCity(), $user->getCountry()))
+            ;
+
+            $this->slackClient->sendMessage($message);
+        } catch (\Exception $e) {
+        } catch (\Http\Client\Exception $e) {
+        }
+
+        try {
+            $this->mailer->sendWelcomeEmailMessage($user);
+        } catch (\Exception $e) {
+        }
     }
 }
