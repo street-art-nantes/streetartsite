12ec9cda34bc640a5462caaeba088f866fd5925d
Julien PETIT <contact@julienpetit.fr>
2019-11-23 14:27:10 +0100

    fix fixtures

---
+++ src/Controller/API/GetArtworksContributedController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Controller\AbstractController;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class MeArtworksController.
+ */
+class MeArtworksController extends AbstractController
+{
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $request->attributes->add(['_resource' => 'User']);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->forward('Api', ['request' => $request, 'id' => $user->getId()]);
+    }
+}
